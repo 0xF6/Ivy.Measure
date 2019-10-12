045d211ddae3ddabf6e2bcba1f5de9fc916402c8
Yuuki Wesp <ls-micro@ya.ru>
2019-10-12 05:25:32 +0300

    add IMeasureFactory

---
+++ src/Ivy.Measure/IMeasureFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Ivy.Measure
+{
+    using System;
+    using JetBrains.Annotations;
+    [PublicAPI]
+    public interface IMeasureFactory<Q> where Q : class, IQuantity<Q>
+    {
+        Q New(float amount);
+        Q New(float amount, IUnit<Q> unit);
+        IMeasure<Q> NewPreserveUnit(float amount, IUnit<Q> unit);
+    }
+    [PublicAPI]
+    public interface IMeasure<Q> : IMeasure, IComparable<IMeasure<Q>>, IEquatable<IMeasure<Q>>
+        where Q : class, IQuantity<Q>
+    {
+        new IUnit<Q> Unit { get; }
+        float GetAmount(IUnit<Q> unit);
+        IMeasure<Q> this[IUnit<Q> iUnit] { get; }
+    }
+
+    public interface IMeasure : IComparable<IMeasure>, IEquatable<IMeasure>
+    {
+        float Amount { get; }
+        float StandardAmount { get; }
+        IUnit Unit { get; }
+        IMeasure this[IUnit iUnit] { get; }
+    }
+}
