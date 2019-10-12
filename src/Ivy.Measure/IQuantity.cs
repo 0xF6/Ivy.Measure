2bacde201a6684a2b2119039247f6cb88bc11eae
Yuuki Wesp <ls-micro@ya.ru>
2019-10-12 05:25:19 +0300

    add IQuantity

---
+++ src/Ivy.Measure/IQuantity.cs
@@ -0,0 +1,15 @@
+﻿namespace Ivy.Measure
+{
+    using System;
+
+    public interface IQuantity : IEquatable<IQuantity>
+    {
+        IUnit StandardUnit { get; }
+        string DisplayFullName { get; }
+    }
+    public interface IQuantity<R> : IQuantity where R : class, IQuantity<R>
+    {
+        new IUnit<R> StandardUnit { get; }
+        IMeasureFactory<R> Factory { get; }
+    }
+}
