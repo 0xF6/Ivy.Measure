64400ecce1472c576511bde36f211478ac027f05
Yuuki Wesp <ls-micro@ya.ru>
2019-10-12 04:44:36 +0300

    update

--- src/Ivy.Measure/MetricPrefix.cs
+++ src/Ivy.Measure/MetricPrefix.cs
@@ -4,49 +4,51 @@ namespace Ivy.Measure
     using System;
     using System.Linq;
     using System.Reflection;
+    using JetBrains.Annotations;
 
+    [PublicAPI]
     public enum MetricPrefix
     {
         [MetricMetadata('y', -24)]
         Yocto,
         [MetricMetadata('z', -21)]
-        Zepto = -21,
+        Zepto,
         [MetricMetadata('a', -18)]
-        Atto = -18,
+        Atto,
         [MetricMetadata('f', -15)]
-        Femto = -15,
+        Femto,
         [MetricMetadata('p', -12)]
-        Pico = -12,
+        Pico,
         [MetricMetadata('n', -9)]
-        Nano = -9,
+        Nano,
         [MetricMetadata('µ', -6)]
-        Micro = -6,
+        Micro,
         [MetricMetadata('m', -3)]
-        Milli = -3,
+        Milli,
         [MetricMetadata('c', -2)]
-        Centi = -2,
+        Centi,
         [MetricMetadata('d', -1)]
-        Deci = -1,
+        Deci,
         [MetricMetadata('D', 1)]
-        Deka = 1,
+        Deka,
         [MetricMetadata('h', 2)]
-        Hecto = 2,
+        Hecto,
         [MetricMetadata('k', 3)]
-        Kilo = 3,
+        Kilo,
         [MetricMetadata('M', 6)]
-        Mega = 6,
+        Mega,
         [MetricMetadata('G', 9)]
-        Giga = 9,
+        Giga,
         [MetricMetadata('T', 12)]
-        Tera = 12,
+        Tera,
         [MetricMetadata('P', 15)]
-        Peta = 15,
+        Peta,
         [MetricMetadata('E', 18)]
-        Exa = 18,
+        Exa,
         [MetricMetadata('Z', 21)]
-        Zetta = 21,
+        Zetta,
         [MetricMetadata('Y', 24)]
-        Yotta = 24
+        Yotta
     }
 
     public static class MetricPrefixExtensions
